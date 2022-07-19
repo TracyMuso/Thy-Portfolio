cb381f5bcadf22637f0ae1d8cbe566a9c6f8348a
Eva Lavinia Bucur <64814566+BucurEva87@users.noreply.github.com>
2022-07-19 13:04:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const hamburger = document.querySelectorAll('.hamMenu')[1];
-const burgerMenu = document.querySelector('.navItems');
+const qs = function(selector = '*', element = document) {
+  return element.querySelector(selector);
+}
 
-hamburger.addEventListener('click', function(){
-  burgerMenu.classList.add('large')
-})
+const qsa = function(selector = '*', element = document) {
+  return [...element.querySelectorAll(selector)];
+}
+
+const hamburger = qs('.hamMenu');
+const hamburgerIcon = qs('.bi-list', hamburger);
+const hamburgerClose = qs('.bi-x', hamburger);
+const burgerMenu = qs('.navItems');
+
+const toggleBurger = function() {
+  hamburgerIcon.classList.toggle('no-display');
+  hamburgerClose.classList.toggle('no-display');
+  burgerMenu.classList.toggle('large');
+}
+
+hamburgerIcon.addEventListener('click', function(){
+  toggleBurger();
+});
+
+hamburgerClose.addEventListener('click', () => {
+  toggleBurger();
+});
+
+qs('.navItems').addEventListener('click', e => {
+  const { target } = e;
+
+  if (target.tagName.toLowerCase() !== 'a') return
+
+  toggleBurger();
+  qs(target.getAttribute('href')).scrollIntoView();
+});
