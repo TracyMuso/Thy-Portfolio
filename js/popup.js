1cc101b282eff3aa5032a3b46ce9b1a47f1d3e89
Elfin Git <musongoletracy@gmail.com>
2022-07-22 00:22:51 +0300

    Adjust spacing of modal elements

--- js/popup.js
+++ js/popup.js
@@ -14,9 +14,9 @@ const works1 = {
 
 const works2 = {
   projectImage: './images/Snapshoot-Portfolio1.svg',
-  Title: 'Project name goes here',
+  Title: 'Multi-post Story',
   Skills: '&#124; html &#124; Ruby on Rails &#124; css &#124; Github &#124;',
-  firstText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industr Lorem Ipsum has been the industrys standard dummy text ever since the 1500swhen an unknown printer took a galley of type and scrambled it 1960s with thereleaLorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,  when an unknown Lorem Ipsum is simply dummy text of the printing and',
+  firstText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scramble',
   secondText: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the.',
   liveDemo: 'See live',
   DemoSource: 'See Source',
@@ -27,7 +27,7 @@ const works2 = {
 
 const works3 = {
   projectImage: './images/Snapshoot-Portfolio1.svg',
-  Title: 'Project name goes here',
+  Title: 'Multi-post Story',
   Skills: '&#124; html &#124; Ruby on Rails &#124; css &#124; Github &#124;',
   firstText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industr Lorem Ipsum has been the industrys standard dummy text ever since the 1500swhen an unknown printer took a galley of type and scrambled it 1960s with thereleaLorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,  when an unknown Lorem Ipsum is simply dummy text of the printing and',
   secondText: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the.',
@@ -40,7 +40,7 @@ const works3 = {
 
 const works4 = {
   projectImage: './images/Snapshoot-Portfolio1.svg',
-  Title: 'Project name goes here',
+  Title: 'Multi-post Story',
   Skills: '&#124; html &#124; Ruby on Rails &#124; css &#124; Github &#124;',
   firstText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industr Lorem Ipsum has been the industrys standard dummy text ever since the 1500swhen an unknown printer took a galley of type and scrambled it 1960s with thereleaLorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,  when an unknown Lorem Ipsum is simply dummy text of the printing and',
   secondText: 'Lorem Ipsum is simply dummy text of the printingand typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the.',
@@ -64,9 +64,9 @@ window.addEventListener('load', () => {
 </div>
 <section id="work-grid-one">
 <div class="project-grid">
-  <img class="project-img" alt="project pic" src="https://schemazone.com/wp-content/uploads/2021/03/Project-Manager.jpg">
+  <img class="project-img" alt="project pic" src="images/Snapshoot-Portfolio2.svg">
     <div class="img-text">
-      <h3 class="project-title">Multi-post Stories</h3>
+      <h3 class="project-title">${worksArr[i].Title}</h3>
        <p class="project-intro">A daily selection of privately personalized reads;
        no accounts or sign-ups required.
        has been the industry's standard dummy text ever since the 1500s,
@@ -87,7 +87,7 @@ window.addEventListener('load', () => {
 <section id="work-grid-two">
 <div class="project-grid">
     <div class="img-text">
-      <h3 class="project-title">Multi-post Stories</h3>
+      <h3 class="project-title">${worksArr[i].Title}</h3>
        <p class="project-intro">A daily selection of privately personalized reads;
        no accounts or sign-ups required.
        has been the industry's standard dummy text ever since the 1500s,
@@ -103,15 +103,15 @@ window.addEventListener('load', () => {
     </ul>
       <button type="button" class="btn" onclick="">See project</button>
    </div>
-  <img class="project-img" alt="project pic" src="https://schemazone.com/wp-content/uploads/2021/03/Project-Manager.jpg">
+  <img class="project-img" alt="project pic" src="images/Snapshoot-Portfolio2.svg">
 </div>
 </section>
 </section>
 <section id="work-grid-three">
 <div class="project-grid">
-  <img class="project-img" alt="project pic" src="https://schemazone.com/wp-content/uploads/2021/03/Project-Manager.jpg">
+  <img class="project-img" alt="project pic" src="images/Snapshoot-Portfolio2.svg">
     <div class="img-text">
-      <h3 class="project-title">Multi-post Stories</h3>
+      <h3 class="project-title">${worksArr[i].Title}</h3>
        <p class="project-intro">A daily selection of privately personalized reads;
        no accounts or sign-ups required.
        has been the industry's standard dummy text ever since the 1500s,
@@ -132,7 +132,7 @@ window.addEventListener('load', () => {
 <section id="work-grid-four">
 <div class="project-grid">
     <div class="img-text">
-      <h3 class="project-title">Multi-post Stories</h3>
+      <h3 class="project-title">${worksArr[i].Title}</h3>
        <p class="project-intro">A daily selection of privately personalized reads;
        no accounts or sign-ups required.
        has been the industry's standard dummy text ever since the 1500s,
@@ -148,7 +148,7 @@ window.addEventListener('load', () => {
     </ul>
      <button type="button" class="btn" onclick="">See project</button>
    </div>
-   <img class="project-img" alt="project pic" src="https://schemazone.com/wp-content/uploads/2021/03/Project-Manager.jpg">
+   <img class="project-img" alt="project pic" src="images/Snapshoot-Portfolio2.svg">
  </div>
 </section>
 `;
@@ -163,15 +163,9 @@ for (let i = 0; i < worksArr.length; i += 1) {
      </div>
      <div class="images-container">
     <div class="PI"></div>
-    <div class="other-works">
-      <div class="first-project"></div>
-      <div class="first-project"></div>
-      <div class="first-project"></div>
-      <div class="first-project"></div>
-    </div>
-    </div>
-    <span onclick="function next()"class=""><i class="bi bi-chevron-compact-right"></i></span>
-    <span onclick="function prev()"class=""><i class="bi bi-chevron-compact-left"></i></span>
+    
+    <span onclick="function next()" class=""><i class="bi bi-chevron-compact-right"></i></span>
+    <span onclick="function prev()" class=""><i class="bi bi-chevron-compact-left"></i></span>
      <p class="workText">${worksArr[i].firstText}</p>
      <p class="workText2">${worksArr[i].secondText}</p>
 
@@ -189,8 +183,9 @@ for (let i = 0; i < worksArr.length; i += 1) {
           </button>
          </div>
          <div class="go-to">
-            <span onclick="function prev()"class="previous">${worksArr[i].previousProject}</span>
+            <span onclick="function prev()" class="previous">${worksArr[i].previousProject}</span>
             <span onclick="function next()" class="next">${worksArr[i].nextProject}</span>
+         </div>
          </div>
          </div>
 `;
